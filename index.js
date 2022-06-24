34698ef166480cd703641cdde15d7f30c0b83483
Renato Martins de Almeida <renato.almeida@enjoei.com.br>
2022-06-24 00:25:05 -0300

    fix: browser configs

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
 const venom = require('venom-bot')
 
-venom.create().then((client) => start(client))
+venom
+  .create(
+    'sessionName',
+    (base64Qrimg, asciiQR) => {
+      console.log('Terminal qrcode: ', asciiQR)
+      console.log('base64 image string qrcode: ', base64Qrimg)
+    },
+    (statusSession) => {
+      console.log('Status Session: ', statusSession) //return isLogged || notLogged || browserClose || qrReadSuccess || qrReadFail || autocloseCalled
+    },
+    {
+      folderNameToken: 'tokens', //folder name when saving tokens
+      mkdirFolderToken: '', //folder directory tokens, just inside the venom folder, example:  { mkdirFolderToken: '/node_modules', } //will save the tokens folder in the node_modules directory
+      headless: true, // Headless chrome
+      devtools: false, // Open devtools by default
+      useChrome: true, // If false will use Chromium instance
+      debug: false, // Opens a debug session
+      logQR: true, // Logs QR automatically in terminal
+      browserArgs: ['--no-sandbox', '--disable-setuid-sandbox'], // Parameters to be added into the chrome browser instance
+      disableSpins: true, // Will disable Spinnies animation, useful for containers (docker) for a better log
+      disableWelcome: true, // Will disable the welcoming message which appears in the beginning
+      updates: true, // Logs info updates automatically in terminal
+      autoClose: 60000 // Automatically closes the venom-bot only when scanning the QR code (default 60 seconds, if you want to turn it off, assign 0 or false)
+    }
+  )
+  .then((client) => {
+    start(client)
+  })
+  .catch((erro) => {
+    console.log(erro)
+  })
 
 let memoryUsers = new Map()
 

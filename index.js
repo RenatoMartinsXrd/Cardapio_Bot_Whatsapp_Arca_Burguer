c7496f26f278dbd288db52ebd3c5ea4eac90ffc2
Renato Martins de Almeida <renato.almeida@enjoei.com.br>
2022-06-23 23:54:40 -0300

    feat: second message

--- index.js
+++ index.js
@@ -3,13 +3,20 @@ const venom = require('venom-bot')
 venom.create().then((client) => start(client))
 
 let memoryUsers = new Map()
+
+function getMinutesDiffDate(datetime2, datetime1) {
+  const miliseconds = datetime2.getTime() - datetime1.getTime()
+  const seconds = miliseconds / 1000
+  const minutes = seconds / 60
+
+  return Math.abs(Math.round(minutes))
+}
+
 function start(client) {
   setInterval(function () {
-    var agora = new Date()
-    var a = `${agora}`
-    var resultado = a.split(' ')
-    console.log(resultado[4])
-    if (resultado[4] == '01:59:00') {
+    let agora = new Date()
+    var resultado = `${agora}`.split(' ')
+    if (resultado[4] == '06:00:00') {
       memoryUsers = new Map()
     }
   }, 1000)
@@ -23,7 +30,25 @@ function start(client) {
             'Olá! Tudo bem com você? Acesse nosso cardápio: https://l.instagram.com/?u=https%3A%2F%2Fapp.anota.ai%2FAArcaBurguereAcai&e=ATOa5tPpQZfk2aEfd_hKeLdunyJBeDcPwhhQAPV40HQEKOPmH_Vafkz08cEDKy8YxpCuYlyuu8Pfc85hjz4tRwWRneSfBY_urcvnufU&s=1'
           )
           .then((result) => {
-            memoryUsers.set(message.from, true)
+            memoryUsers.set(message.from, {
+              hora: new Date(),
+              finalizado: false
+            })
+          })
+          .catch((erro) => {
+            console.error('Erro ao enviar mensagem: ', erro)
+          })
+      } else if (
+        !memoryUsers.get(message.from).finalizado &&
+        getMinutesDiffDate(new Date(), memoryUsers.get(message.from).hora) >= 1
+      ) {
+        client
+          .sendText(
+            message.from,
+            'Olá, caso queira falar conosco ligue para nosso whatsapp'
+          )
+          .then((result) => {
+            memoryUsers.get(message.from).finalizado = true
           })
           .catch((erro) => {
             console.error('Erro ao enviar mensagem: ', erro)
